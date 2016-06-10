86add42aabdb07495b2e6b28c28163d41983fb6d
fenglei <fenglei@baidu.com>
2016-06-10 11:28:03 +0800

    m

--- shaman-example/src/test/java/org/shaman/example/CUstomQueryExampleTest.java
+++ shaman-example/src/test/java/org/shaman/example/CUstomQueryExampleTest.java
@@ -1,7 +1,13 @@
 package org.shaman.example;
 
+
+
 /**
  * Created by fenglei on 2016/6/10.
  */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations ={"classpath:/configuration/spring/beans.xml"})
+//@Transactional
+//@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
 public class CustomQueryExampleTest {
 }
