8ad1d3fd13b9cef73625c9342a2a60c9bbe75fbc
fenglei <fenglei@baidu.com>
2016-06-10 16:00:21 +0800

    add h2 pom config

--- shaman-example/src/test/java/org/shaman/example/BaseTest.java
+++ shaman-example/src/test/java/org/shaman/example/BaseTest.java
@@ -1,7 +1,21 @@
 package org.shaman.example;
 
+import org.junit.After;
+import org.junit.Before;
+
 /**
  * Created by fenglei on 2016/6/10.
  */
 public class BaseTest {
+
+    @Before
+    public void before() {
+        H2DataBase.start();
+
+    }
+
+    @After
+    public void after() {
+        H2DataBase.stop();
+    }
 }
--- shaman-example/src/test/java/org/shaman/example/H2DataBase.java
+++ shaman-example/src/test/java/org/shaman/example/H2DataBase.java
@@ -1,10 +1,42 @@
 package org.shaman.example;
 
-//import org.h2.tools.Server;
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
+
 /**
  * Created by fenglei on 2016/6/10.
  */
 public class H2DataBase {
 
+    private static Server server;
 
+    /**
+     *  start start
+     */
+    public static void start() {
+        try {
+            System.out.println("正在启动h2...");
+            server = Server.createTcpServer(
+                    new String[]{"-tcp", "-tcpAllowOthers", "-tcpPort",
+                            "8043"}).start();
+            System.out.println("启动成功：" + server.getStatus());
+        } catch (SQLException e) {
+            System.out.println("启动h2出错：" + e.toString());
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * stop stop
+     *
+     */
+    public static void stop() {
+        if (server != null) {
+            System.out.println("正在关闭h2...");
+            server.stop();
+            System.out.println("关闭成功.");
+        }
+    }
 }
