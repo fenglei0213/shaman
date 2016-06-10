4b2f3dc2e04d303c956aa4dcdacc8721aa68da87
fenglei <fenglei@baidu.com>
2016-06-10 11:55:57 +0800

    m

--- shaman-example/src/test/java/org/shaman/example/CUstomQueryExampleTest.java
+++ shaman-example/src/test/java/org/shaman/example/CUstomQueryExampleTest.java
@@ -1,13 +1,18 @@
 package org.shaman.example;
 
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by fenglei on 2016/6/10.
  */
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations ={"classpath:/configuration/spring/beans.xml"})
-//@Transactional
-//@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations ={"classpath:/init.xml"})
+@Transactional(transactionManager = "transactionManager")
 public class CustomQueryExampleTest {
+
+
 }
