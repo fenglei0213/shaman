2dc633e537fbe9eb580d1e6f35724853ad79ddee
fenglei <fenglei0213@163.com>
2016-12-18 10:17:44 +0800

    update genInsertBatchSQL ,extract genIsertBatchSQL From partition Method

--- shaman-core/src/main/java/org/shaman/util/ShamanSQLUtils.java
+++ shaman-core/src/main/java/org/shaman/util/ShamanSQLUtils.java
@@ -18,36 +18,49 @@ public class ShamanSQLUtils {
      * @param partitionSize
      * @return
      */
-    public static List<String> genInsertBatchSQL(String targetTableName,
-                                                 List<String> lineAllList,
-                                                 int partitionSize) {
+    public static List<String> genInsertBatchSQLPartition(String targetTableName,
+                                                          List<String> lineAllList,
+                                                          int partitionSize) {
         List<String> sqlList = Lists.newArrayList();
         List<List<String>> lineListList = Lists.partition(lineAllList, partitionSize);
         for (List<String> lineList : lineListList) {
-            StringBuilder sqlSb = new StringBuilder("INSERT"
-                    + " INTO " + targetTableName + " VALUES ");
-            List<String> columnValueAllList = Lists.newArrayList();
-            for (String line : lineList) {
-                String[] lineArray = line.split("\t");
-                List<String> columnValueItemList = Lists.newArrayList();
-                for (String item : lineArray) {
-                    if (StringUtils.isEmpty(item)) {
-                        columnValueItemList.add("null");
-                        continue;
-                    }
-                    columnValueItemList.add("'" + item + "'");
-                }
-                String columnValueItemListString =
-                        "(" + StringUtils.join(columnValueItemList,
-                                ",") + ")";
-                columnValueAllList.add(columnValueItemListString);
-            }
-            String columnValueAllListString = StringUtils.join(columnValueAllList,
-                    ",");
-            sqlSb.append(columnValueAllListString);
-            String sql = sqlSb.toString();
+            String sql = ShamanSQLUtils.genInsertBatchSQL(targetTableName, lineList);
             sqlList.add(sql);
         }
         return sqlList;
+    }
+
+    /**
+     * genInsertBatchSQL genInsertBatchSQL
+     *
+     * @param targetTableName
+     * @param lineList
+     * @return
+     */
+    public static String genInsertBatchSQL(String targetTableName,
+                                           List<String> lineList) {
+        StringBuilder sqlSb = new StringBuilder("INSERT"
+                + " INTO " + targetTableName + " VALUES ");
+        List<String> columnValueAllList = Lists.newArrayList();
+        for (String line : lineList) {
+            String[] lineArray = line.split("\t");
+            List<String> columnValueItemList = Lists.newArrayList();
+            for (String item : lineArray) {
+                if (StringUtils.isEmpty(item)) {
+                    columnValueItemList.add("null");
+                    continue;
+                }
+                columnValueItemList.add("'" + item + "'");
+            }
+            String columnValueItemListString =
+                    "(" + StringUtils.join(columnValueItemList,
+                            ",") + ")";
+            columnValueAllList.add(columnValueItemListString);
+        }
+        String columnValueAllListString = StringUtils.join(columnValueAllList,
+                ",");
+        sqlSb.append(columnValueAllListString);
+        String sql = sqlSb.toString();
+        return sql;
     }
 }
