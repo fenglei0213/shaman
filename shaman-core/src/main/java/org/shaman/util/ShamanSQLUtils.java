89aa5c431a8c662f220b81ac4e884dde11872a20
fenglei <fenglei0213@163.com>
2016-12-18 09:33:12 +0800

    update genInsertBatchSQL , add Partition Feature

--- shaman-core/src/main/java/org/shaman/util/ShamanSQLUtils.java
+++ shaman-core/src/main/java/org/shaman/util/ShamanSQLUtils.java
@@ -14,32 +14,40 @@ public class ShamanSQLUtils {
      * genInsertBatchSQL genInsertBatchSQL
      *
      * @param targetTableName
-     * @param lineList
+     * @param lineAllList
+     * @param partitionSize
      * @return
      */
-    public static String genInsertBatchSQL(String targetTableName, List<String> lineList) {
-        StringBuilder sqlSb = new StringBuilder("INSERT"
-                + " INTO " + targetTableName + " VALUES ");
-        List<String> columnValueAllList = Lists.newArrayList();
-        for (String line : lineList) {
-            String[] lineArray = line.split("\t");
-            List<String> columnValueItemList = Lists.newArrayList();
-            for (String item : lineArray) {
-                if (StringUtils.isEmpty(item)) {
-                    columnValueItemList.add("null");
-                    continue;
+    public static List<String> genInsertBatchSQL(String targetTableName,
+                                                 List<String> lineAllList,
+                                                 int partitionSize) {
+        List<String> sqlList = Lists.newArrayList();
+        List<List<String>> lineListList = Lists.partition(lineAllList, partitionSize);
+        for (List<String> lineList : lineListList) {
+            StringBuilder sqlSb = new StringBuilder("INSERT"
+                    + " INTO " + targetTableName + " VALUES ");
+            List<String> columnValueAllList = Lists.newArrayList();
+            for (String line : lineList) {
+                String[] lineArray = line.split("\t");
+                List<String> columnValueItemList = Lists.newArrayList();
+                for (String item : lineArray) {
+                    if (StringUtils.isEmpty(item)) {
+                        columnValueItemList.add("null");
+                        continue;
+                    }
+                    columnValueItemList.add("'" + item + "'");
                 }
-                columnValueItemList.add("'" + item + "'");
+                String columnValueItemListString =
+                        "(" + StringUtils.join(columnValueItemList,
+                                ",") + ")";
+                columnValueAllList.add(columnValueItemListString);
             }
-            String columnValueItemListString =
-                    "(" + StringUtils.join(columnValueItemList,
-                            ",") + ")";
-            columnValueAllList.add(columnValueItemListString);
+            String columnValueAllListString = StringUtils.join(columnValueAllList,
+                    ",");
+            sqlSb.append(columnValueAllListString);
+            String sql = sqlSb.toString();
+            sqlList.add(sql);
         }
-        String columnValueAllListString = StringUtils.join(columnValueAllList,
-                ",");
-        sqlSb.append(columnValueAllListString);
-        String sql = sqlSb.toString();
-        return sql;
+        return sqlList;
     }
 }
